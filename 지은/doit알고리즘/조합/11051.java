a552a210483d20188c8b3fcf312caa5c3f789fbc
JiELee1 <dlwldmscjstk@naver.com>
2024-12-20 15:04:21 +0900

    doit 조합

---
+++ 지은/doit알고리즘/조합/11051.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int[][] c = new int[n+1][n+1];
+        for(int i = 0; i<c.length; i++) {
+            c[i][i] = 1;
+            c[i][1] = i;
+            c[i][0] = 1;
+        }
+        
+        for(int i = 1; i<c.length; i++) {
+            for(int j =1; j<i; j++) {
+                c[i][j] = (c[i-1][j]+c[i-1][j-1])%10007;
+            }
+        }
+        
+        bw.write(String.valueOf(c[n][k]));
+        bw.flush();
+        bw.close();
+    }
+}
