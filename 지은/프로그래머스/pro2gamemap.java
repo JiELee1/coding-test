431f14ae61f9e79646fa93231052a8000870fd25
JiELee1 <dlwldmscjstk@naver.com>
2025-03-02 20:43:09 +0900

    프로그래머스 게임 맵 최단거리(bfs)

--- 지은/프로그래머스/pro2gamemap.java
+++ 지은/프로그래머스/pro2gamemap.java
@@ -1,5 +1,56 @@
 import java.util.*;
 
+
+import java.util.*;
+
+class Solution {
+    int[] dx = {1,0,-1,0};
+    int[] dy = {0,1,0,-1};
+    int[][] visit;
+    int xMax, yMax;
+    public int solution(int[][] maps) {
+        xMax = maps[0].length;
+        yMax = maps.length;
+        visit = new int[yMax][xMax];
+        visit[0][0] = 1;
+        bfs(maps);
+        return visit[yMax-1][xMax-1] == 0 ? -1 : visit[yMax-1][xMax-1];
+    }
+    
+    void bfs(int[][] maps) {
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(0,0,1));
+        
+        while(!queue.isEmpty()) {
+            Node node = queue.removeFirst();
+            for(int i = 0; i<4; i++) {
+                int nx = node.x+dx[i];
+                int ny = node.y+dy[i];
+
+                if(nx>=0 && ny>=0 && nx<xMax && ny<yMax) {
+                    if(maps[ny][nx] == 1 && visit[ny][nx] == 0) {
+                        visit[ny][nx] = node.dis+1;
+                        queue.add(new Node(nx, ny, node.dis+1));
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
+class Node {
+    int x, y, dis;
+    
+    public Node(int x, int y, int dis) {
+        this.x = x;
+        this.y = y;
+        this.dis = dis;
+    }
+}
+
+
+//dfs (효율성 테스트에서 실패함)
 class Solution {
     int[] dx = {1,0,-1,0};
     int[] dy = {0,1,0,-1};
