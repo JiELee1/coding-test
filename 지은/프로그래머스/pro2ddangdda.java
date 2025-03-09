7cd53dd22180b76f4069a4be140476b807a30ce4
JiELee1 <dlwldmscjstk@naver.com>
2025-03-09 16:30:45 +0900

    프로그래머스 땅따먹기(dp)

--- 지은/프로그래머스/pro2ddangdda.java
+++ 지은/프로그래머스/pro2ddangdda.java
@@ -1,3 +1,27 @@
+//dp
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        int[][] dp = new int[land.length+1][4];
+        for(int i=1; i<=land.length; i++) {
+            for(int j=0; j<4; j++) { //이전꺼   
+                for(int k=0; k<4; k++) {
+                    if(j != k) {
+                        dp[i][k] = Math.max(dp[i-1][j] + land[i-1][k], dp[i][k]);
+                    }
+                    answer = Math.max(answer, dp[i][k]);
+                }
+            }
+
+        }
+        return answer;
+    }
+
+}
+
+
+
+//시간초과
 class Solution {
     int result = 0;
     int solution(int[][] land) {
